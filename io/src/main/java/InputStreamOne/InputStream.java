1ca1ef63fe5af08279eabe7705625b31b603d431
weiShuai <601770050@qq.com>
2018-03-16 08:27:10 +0800

    新增字节流读取操作

---
+++ io/src/main/java/InputStreamOne/InputStream.java
@@ -0,0 +1,27 @@
+package InputStreamOne;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+/**
+ * Created by WS on 2018/3/16.
+ */
+public class InputStream {
+
+    public static void main(String[] args) {
+        File file = new File("E:\\weiShuai\\IO\\source.txt");
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(file);
+            byte[] bytes = new byte[1024];
+            int len = 0;
+            while ((len = fileInputStream.read(bytes)) != -1) {
+                String str = new String(bytes, 0, len);
+                System.out.println(str);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
