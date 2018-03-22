42960e7896f71c3715e53263e4d8b46c8d75d1be
weiShuai <601770050@qq.com>
2018-03-22 08:00:19 +0800

    修改类名

--- io/src/main/java/randonAccessFile/testCopyOfRandomAccessFile.java
+++ io/src/main/java/randonAccessFile/testCopyOfRandomAccessFile.java
@@ -1,7 +1,6 @@
 package randonAccessFile;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
@@ -10,13 +9,13 @@ import java.io.RandomAccessFile;
  *
  * Created by WS on 2018/3/22.
  */
-public class testOfRandomAccessFile {
+public class testCopyOfRandomAccessFile {
     public static void main(String[] args) {
         RandomAccessFile randomAccessFile01 = null;
         RandomAccessFile randomAccessFile02 = null;
         try {
             randomAccessFile01 = new RandomAccessFile(new File("E:\\weiShuai\\IO\\source.txt"), "r");
-            randomAccessFile02 = new RandomAccessFile(new File("E:\\weiShuai\\IO\\testOfRandomAccessFile.txt"), "rw");
+            randomAccessFile02 = new RandomAccessFile(new File("E:\\weiShuai\\IO\\testCopyOfRandomAccessFile.txt"), "rw");
             byte[] bytes = new byte[10];
             int len;
             while ((len = randomAccessFile01.read(bytes)) != -1) {
